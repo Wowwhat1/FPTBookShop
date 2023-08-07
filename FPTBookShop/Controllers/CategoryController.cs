b1e1c73f59426bb957d2cbd9cca3bc6f67d47452
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-07 20:45:18 +0700

    Create function Add Category

--- FPTBookShop/Controllers/CategoryController.cs
+++ FPTBookShop/Controllers/CategoryController.cs
@@ -1,10 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FPTBookShop.Data;
+using FPTBookShop.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FPTBookShop.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
