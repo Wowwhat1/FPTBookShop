2bc180fa18eaf6c4e533db5b047253368c9abfe7
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-14 10:39:29 +0700

    Add Product Repository

---
+++ FPTBookShop.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using FPTBookShop.DataAccess.Data;
+using FPTBookShop.DataAccess.Repository.IRepository;
+using FPTBookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPTBookShop.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
