137fe1cea7fc04e13fc759b3fa20d124dac147ab
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-16 15:37:37 +0700

    Add Delete Product

--- FPTBookShop/Areas/Admin/Controllers/ProductController.cs
+++ FPTBookShop/Areas/Admin/Controllers/ProductController.cs
@@ -102,40 +102,34 @@ namespace FPTBookShop.Areas.Admin.Controllers
             }
         }
 
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            Product productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePOST(int? id)
-        {
-            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            _unitOfWork.Product.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Product deleted successful";
-            return RedirectToAction("Index");
-        }
-
         #region APICALLS
+        [HttpGet]
         public IActionResult GetAll()
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return Json(new {data =  objProductList});
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var productToBeDelete = _unitOfWork.Product.Get(u => u.Id == id);
+
+            if (productToBeDelete == null)
+            {
+                return Json(new { success = false, message = "Error while deleting!" });
+            }
+
+            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productToBeDelete.ImageUrl.TrimStart('\\'));
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath); 
+            }
+
+            _unitOfWork.Product.Remove(productToBeDelete);  
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete successful!" });
         }
         #endregion
     }
--- FPTBookShop/wwwroot/js/product.js
+++ FPTBookShop/wwwroot/js/product.js
@@ -6,11 +6,21 @@ function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/product/getall' },
         "columns": [
-            { data: 'title', "width": "15%" },
-            { data: 'isbn', "width": "15%" },
-            { data: 'listPrice', "width": "15%" },
+            { data: 'title', "width": "20%" },
+            { data: 'isbn', "width": "20%" },
+            { data: 'listPrice', "width": "10%" },
             { data: 'author', "width": "15%" },
-            { data: 'category.name', "width": "15%" }
+            { data: 'category.name', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-100 d-flex justify-content-between" role="group">
+                    <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"> </i> Edit </a>
+                    <a href="/admin/product/delete?id=${data}" class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i>Delete</a>
+                    </div>`
+                },
+                "width": "25%"
+            }
         ]
     });
 }
