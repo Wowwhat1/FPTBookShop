dc7f7bfee69c9f2240b3d8a4ee937b19f1f8bc50
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-18 00:58:11 +0700

    Add function Add to cart

--- FPTBookShop/Areas/Customer/Controllers/HomeController.cs
+++ FPTBookShop/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using FPTBookShop.DataAccess.Repository.IRepository;
 using FPTBookShop.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace FPTBookShop.Areas.Customer.Controllers
 {
@@ -34,6 +36,19 @@ namespace FPTBookShop.Areas.Customer.Controllers
             return View(cart);
         }
 
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
 
         public IActionResult Privacy()
         {
