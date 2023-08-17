274516dcf0bb723bb7e60072815c8ac6b8c3eba8
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-17 23:46:57 +0700

    Add Shopping Cart model and add it to Repository

---
+++ FPTBookShop.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,14 @@
+﻿using FPTBookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPTBookShop.DataAccess.Repository.IRepository
+{
+    public interface IShoppingCartRepository : IRepository<ShoppingCart>
+    {
+        void Update(ShoppingCart obj);
+    }
+}
