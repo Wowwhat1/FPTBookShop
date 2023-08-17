ae62e9098377d321e23dfbfc5af0d210c810323a
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-18 02:18:23 +0700

    Add Update Quantity in Shopping Cart

--- FPTBookShop/Areas/Customer/Controllers/CartController.cs
+++ FPTBookShop/Areas/Customer/Controllers/CartController.cs
@@ -13,14 +13,15 @@ namespace FPTBookShop.Areas.Customer.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         public ShoppingCartVM ShoppingCartVM { get; set; }
-
         public CartController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
 
+
         public IActionResult Index()
         {
+
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
@@ -30,26 +31,66 @@ namespace FPTBookShop.Areas.Customer.Controllers
                 includeProperties: "Product")
             };
 
-            foreach(var cart in ShoppingCartVM.ShoppingCartList)
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
-                cart.Price= GetPriceBaseOnQuantity(cart);
+                cart.Price = GetPriceBasedOnQuantity(cart);
                 ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
             }
 
             return View(ShoppingCartVM);
         }
 
-        private double GetPriceBaseOnQuantity(ShoppingCart shoppingCart)
+
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            if (cartFromDb.Count <= 1)
+            {
+                //remove that from cart
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
         {
             if (shoppingCart.Count <= 50)
             {
-                return shoppingCart.Product.Price;  
-            } else
+                return shoppingCart.Product.Price;
+            }
+            else
             {
                 if (shoppingCart.Count <= 100)
                 {
                     return shoppingCart.Product.Price50;
-                } else
+                }
+                else
                 {
                     return shoppingCart.Product.Price100;
                 }
