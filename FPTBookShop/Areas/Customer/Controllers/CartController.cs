4b1d323e3b9a3dea76b5193515f664d89b79fa70
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-18 01:59:53 +0700

    Get Shopping Cart

--- FPTBookShop/Areas/Customer/Controllers/CartController.cs
+++ FPTBookShop/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FPTBookShop.DataAccess.Repository.IRepository;
+using FPTBookShop.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace FPTBookShop.Areas.Customer.Controllers
 {
     [Area("customer")]
+    [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product")
+            };
+
+            return View(ShoppingCartVM);
         }
     }
 }
