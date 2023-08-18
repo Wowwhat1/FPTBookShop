7174c8a286fc2f62a4843101b4c97723ed34f83a
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-18 20:21:21 +0700

    Add Helper Methods in Order Header Repository

---
+++ FPTBookShop.DataAccess/Migrations/20230818094513_addSessionIdToOrderHeader.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPTBookShop.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSessionIdToOrderHeader : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orderDetail_Products_ProductId",
+                table: "orderDetail");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_orderDetail_orderHeaders_OrderHeaderId",
+                table: "orderDetail");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_orderHeaders_AspNetUsers_ApplicationUserId",
+                table: "orderHeaders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_orderHeaders",
+                table: "orderHeaders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_orderDetail",
+                table: "orderDetail");
+
+            migrationBuilder.RenameTable(
+                name: "orderHeaders",
+                newName: "OrderHeaders");
+
+            migrationBuilder.RenameTable(
+                name: "orderDetail",
+                newName: "OrderDetails");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_orderHeaders_ApplicationUserId",
+                table: "OrderHeaders",
+                newName: "IX_OrderHeaders_ApplicationUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_orderDetail_ProductId",
+                table: "OrderDetails",
+                newName: "IX_OrderDetails_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_orderDetail_OrderHeaderId",
+                table: "OrderDetails",
+                newName: "IX_OrderDetails_OrderHeaderId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SessionId",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderHeaders",
+                table: "OrderHeaders",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderDetails",
+                table: "OrderDetails",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_OrderHeaders_OrderHeaderId",
+                table: "OrderDetails",
+                column: "OrderHeaderId",
+                principalTable: "OrderHeaders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Products_ProductId",
+                table: "OrderDetails",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderHeaders_AspNetUsers_ApplicationUserId",
+                table: "OrderHeaders",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_OrderHeaders_OrderHeaderId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Products_ProductId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderHeaders_AspNetUsers_ApplicationUserId",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderHeaders",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderDetails",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "SessionId",
+                table: "OrderHeaders");
+
+            migrationBuilder.RenameTable(
+                name: "OrderHeaders",
+                newName: "orderHeaders");
+
+            migrationBuilder.RenameTable(
+                name: "OrderDetails",
+                newName: "orderDetail");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderHeaders_ApplicationUserId",
+                table: "orderHeaders",
+                newName: "IX_orderHeaders_ApplicationUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "orderDetail",
+                newName: "IX_orderDetail_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderDetails_OrderHeaderId",
+                table: "orderDetail",
+                newName: "IX_orderDetail_OrderHeaderId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_orderHeaders",
+                table: "orderHeaders",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_orderDetail",
+                table: "orderDetail",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orderDetail_Products_ProductId",
+                table: "orderDetail",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orderDetail_orderHeaders_OrderHeaderId",
+                table: "orderDetail",
+                column: "OrderHeaderId",
+                principalTable: "orderHeaders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orderHeaders_AspNetUsers_ApplicationUserId",
+                table: "orderHeaders",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
