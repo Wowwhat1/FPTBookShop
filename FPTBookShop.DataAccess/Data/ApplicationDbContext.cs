5d6ae249882b23a231af26e3c75d23b9eb8d434c
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-18 10:24:19 +0700

    Create Order Header and Order Detail

--- FPTBookShop.DataAccess/Data/ApplicationDbContext.cs
+++ FPTBookShop.DataAccess/Data/ApplicationDbContext.cs
@@ -17,6 +17,8 @@ namespace FPTBookShop.DataAccess.Data
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<ApplicationUser> applicationUsers { get; set; }
+        public DbSet<OrderHeader> orderHeaders { get; set; }
+        public DbSet<OrderDetail> orderDetail { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
