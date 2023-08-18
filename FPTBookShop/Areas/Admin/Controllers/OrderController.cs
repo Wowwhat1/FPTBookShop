6c39ea80a602ec1d25634ef43dd2ee7717214910
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-19 00:57:36 +0700

    Add OrderVM and Order Controller

---
+++ FPTBookShop/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using FPTBookShop.DataAccess.Repository.IRepository;
+using FPTBookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FPTBookShop.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region APICALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeader });
+        }
+
+        #endregion
+    }
+}
