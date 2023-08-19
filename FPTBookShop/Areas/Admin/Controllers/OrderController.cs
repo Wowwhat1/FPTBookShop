a731f8b78be0c6a6f9871c0a6bceea8722109831
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-19 12:29:17 +0700

    Finishing Status Filter

--- FPTBookShop/Areas/Admin/Controllers/OrderController.cs
+++ FPTBookShop/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using FPTBookShop.DataAccess.Repository.IRepository;
 using FPTBookShop.Models;
+using FPTBookShop.Utility;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FPTBookShop.Areas.Admin.Controllers
@@ -21,10 +22,29 @@ namespace FPTBookShop.Areas.Admin.Controllers
 
         #region APICALLS
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> objOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-            return Json(new { data = objOrderHeader });
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+            return Json(new { data = objOrderHeaders });
         }
 
         #endregion
