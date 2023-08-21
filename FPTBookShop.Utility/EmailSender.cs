b6d48b245567322e975c647881e2bc1d53260885
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-08-21 08:59:30 +0700

    Set up SendEmailGrid

--- FPTBookShop.Utility/EmailSender.cs
+++ FPTBookShop.Utility/EmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +12,23 @@ namespace FPTBookShop.Utility
 {
     public class EmailSender : IEmailSender
     {
+        public string SendGridScret { get; set; }
+
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridScret = _config.GetValue<string>("SendGrid: SecretKey");
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             // logic to send email
-            return Task.CompletedTask;
+            var client = new SendGridClient(SendGridScret);
+
+            var from = new EmailAddress("hello@dotnetmastery.com", "Bulk Book");
+            var to = new EmailAddress(email);
+            var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+            return client.SendEmailAsync(message);
         }
     }
 }
--- FPTBookShop/Program.cs
+++ FPTBookShop/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using FPTBookShop.Utility;
 using Stripe;
+using FPTBookShop.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,6 +38,7 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
@@ -64,6 +66,8 @@ app.UseAuthorization();
 
 app.UseSession();
 
+SeedDatabase();
+
 app.MapRazorPages();
 
 app.MapControllerRoute(
@@ -71,3 +75,12 @@ app.MapControllerRoute(
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
